c1c3cba2c5bfd05ce9c787dc6f74273f4e867fe9
Marcio Cavalcante <mca19ntl@gmail.com>
2021-02-04 00:16:22 -0300

    Projeto criado

---
+++ src/teste_github/Teste_Github.java
@@ -0,0 +1,16 @@
+package teste_github;
+
+public class Teste_Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Teste
+		
+		System.out.println("Bom dia!");
+		// System.out.println("Boa tarde!");
+		// System.out.println("Boa noite!");
+		// System.out.println("Olá!");
+	}
+
+}
